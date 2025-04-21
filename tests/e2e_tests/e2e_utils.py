89cddda64735efc75208b56f05f4504c53930b3a
ishaansehgal99 <ishaanforthewin@gmail.com>
2025-04-21 15:32:56 -0700

    feat: add license

--- tests/e2e_tests/e2e_utils.py
+++ tests/e2e_tests/e2e_utils.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2023 PR-Agent Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 FILE_PATH = "pr_agent/cli_pip.py"
 
 PR_HEADER_START_WITH = '### **User description**\nupdate cli_pip.py\n\n\n___\n\n### **PR Type**'
