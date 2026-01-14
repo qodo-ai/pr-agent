d39f30965bf13c64891901f7d64da75a144d78fa
tanin <taninx@gmail.com>
2026-01-14 16:52:02 +0200

    chore: Remove test file after confirming inline comments work

--- pr_agent/tools/demo_code.py
+++
@@ -1,8 +0,0 @@
-"""Demo file to trigger Python analyzer."""
-
-def demo_function():
-    print("This WILL trigger an analyzer warning!")
-    data = []
-    for i in range(10):
-        data.append(i)
-    return data
